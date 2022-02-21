200db3173d4d5b1bbc18fdfce566b9c31e4695cb
Yosh <izolate@gmail.com>
2022-02-21 22:14:20 +0000

    Add main test file

---
+++ main_test.go
@@ -0,0 +1,70 @@
+package yesql
+
+import (
+	"os"
+	"testing"
+)
+
+const (
+	sqlUp = `
+CREATE TABLE genres (
+    id serial PRIMARY KEY,
+    name text
+);
+
+INSERT INTO genres (name) VALUES ('Fantasy');
+INSERT INTO genres (name) VALUES ('Horror');
+INSERT INTO genres (name) VALUES ('Sci-Fi');
+INSERT INTO genres (name) VALUES ('Self-Help');
+INSERT INTO genres (name) VALUES ('Computers');
+INSERT INTO genres (name) VALUES ('Comedy');
+
+CREATE TABLE authors (
+    id serial PRIMARY KEY,
+    name text
+);
+
+CREATE TABLE books (
+    id serial PRIMARY KEY,
+    title text NOT NULL,
+    author serial REFERENCES authors(id) NOT NULL,
+    genre serial REFERENCES genres(id) NOT NULL
+);`
+
+	sqlDown = `
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS genres;`
+)
+
+var db *DB
+
+type category struct {
+	ID   int    `db:"id"`
+	Name string `db:"name"`
+}
+
+type author struct {
+	ID   int    `db:"id"`
+	Name string `db:"name"`
+}
+
+type book struct {
+	ID     int    `db:"id"`
+	Title  string `db:"title"`
+	Author int    `db:"author"`
+	Genre  int    `db:"genre"`
+}
+
+func mustExec(q string) {
+	if _, err := db.DB.Exec(q); err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	db = MustOpen("postgres", "host=localhost user=postgres password=postgres database=postgres sslmode=disable")
+	mustExec(sqlDown)
+	mustExec(sqlUp)
+	os.Exit(m.Run())
+}
--- yesql_test.go
+++ yesql_test.go
@@ -1,41 +1,10 @@
 package yesql
 
 import (
-	"os"
 	"testing"
 
 	_ "github.com/lib/pq"
 )
-
-var db *DB
-
-type category struct {
-	ID   int    `db:"id"`
-	Name string `db:"name"`
-}
-
-type author struct {
-	ID   int    `db:"id"`
-	Name string `db:"name"`
-}
-
-type book struct {
-	ID     int    `db:"id"`
-	Title  string `db:"title"`
-	Author int    `db:"author"`
-	Genre  int    `db:"genre"`
-}
-
-func TestMain(m *testing.M) {
-	db = MustOpen("postgres", "host=localhost user=postgres password=postgres database=postgres sslmode=disable")
-	if _, err := db.DB.Exec("DELETE FROM books"); err != nil {
-		panic(err)
-	}
-	if _, err := db.DB.Exec("DELETE FROM authors"); err != nil {
-		panic(err)
-	}
-	os.Exit(m.Run())
-}
 
 func TestExec(t *testing.T) {
 	authors := []author{
@@ -52,4 +21,20 @@ func TestExec(t *testing.T) {
 		}
 		t.Log(res.RowsAffected())
 	}
+
+	books := []book{
+		{Title: "A Storm Of Swords", Author: 1, Genre: 1},
+		{Title: "Alice's Adventures In Wonderland", Author: 2, Genre: 1},
+		{Title: "The Fellowship Of The Ring", Author: 3, Genre: 1},
+	}
+
+	for _, b := range books {
+		q := "INSERT INTO books (title, author, genre) VALUES (@Title, @Author, @Genre);"
+		res, err := db.Exec(q, b)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Log(res.RowsAffected())
+	}
+
 }
